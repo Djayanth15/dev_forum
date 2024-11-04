3ad291a5da4f5bf00d536fb8c61ea118e170c6ba
Djay <dannana.jayanth@gmail.com>
2024-11-04 14:35:53 -0500

    tailwind css(theme) setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl font-black">Welcome to the world of next js</h1>
+    <>
+      <h1 className="font-inter text-3xl font-black">
+        Welcome to the world of next js
+      </h1>
+      <h1 className="font-space-grotesk text-3xl font-black">
+        Welcome to the world of next js
+      </h1>
+    </>
   );
 };
 
