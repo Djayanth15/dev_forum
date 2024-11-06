6cfcd53ce0ccdb906506ec28b19e3f96bdb4d2ac
Djay <dannana.jayanth@gmail.com>
2024-11-06 15:28:22 -0500

    Setup Auth layout, Social Auth From, Next Auth, OAuth with Google and github Providers

---
+++ app/(root)/page.tsx
@@ -0,0 +1,30 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+  return (
+    <>
+      <h1 className="m-20 font-inter text-3xl font-black">
+        Welcome to the world of next js
+      </h1>
+      <h1 className="font-space-grotesk text-3xl font-black">
+        Welcome to the world of next js
+      </h1>
+
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Logout</Button>
+      </form>
+    </>
+  );
+};
+
+export default Home;
--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable tailwindcss/no-custom-classname */
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -7,7 +6,7 @@ import Theme from "./Theme";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full gap-5 p-6 sm:px-12 dark:shadow-none">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/images/site-logo.svg"
